fe4edd33e02bfa7dc785a585024aa5d543f9b983
Pablo Aran <pabloarancibia.dw@gmail.com>
2020-04-30 10:14:48 -0300

    clase 44 parte 1

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-footer',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  emailField: FormControl;
 
-  ngOnInit(): void {
+  constructor() {
+    this.emailField = new FormControl('', [
+      Validators.required,
+      Validators.email,
+      Validators.minLength(4),
+      Validators.maxLength(10)
+    ]);
+    // this.emailField.valueChanges
+    // .subscribe(value => {
+    //   console.log(value);
+    // });
+  }
+
+  ngOnInit() {
+  }
+
+  sendMail() {
+    if (this.emailField.valid) {
+      console.log(this.emailField.value);
+    }
   }
 
 }
